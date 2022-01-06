49b6973d71d797b326aeb97d4e55fca525599885
Seongyun Kim <atomyoun@gmail.com>
2022-01-06 19:16:12 +0900

    76501

---
+++ Level 1/pro_76501.java
@@ -0,0 +1,22 @@
+public class pro_76501 {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for (int i = 0 ; i<absolutes.length;i++){
+            if(signs[i]==true){
+                answer = answer + absolutes[i];
+            }else{
+                answer = answer - absolutes[i];
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        pro_76501 p = new pro_76501();
+        int [] absolutes1 = {4,7,12};
+        boolean [] signs1 = {true, false, true};
+        int [] absolutes2 = {1,2,3};
+        boolean [] signs2 = {false, false, true};
+        System.out.println(p.solution(absolutes1, signs1));
+        System.out.println(p.solution(absolutes2, signs2));
+    }
+}
