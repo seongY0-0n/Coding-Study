c547f4162e17ed2c38f8ded22ed3240c11c9fc83
seriokim <atomyoun@gmail.com>
2022-02-16 22:25:06 +0900

    42748

---
+++ Level 1/pro_42748.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class pro_42748 {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for(int i = 0; i<commands.length; i++){
+            int[] temp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2]-1];
+        }
+        return answer;
+    }
+}
