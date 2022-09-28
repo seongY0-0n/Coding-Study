f4ac9dcc26df971e96d722a21475dd60678ee766
seriokim <atomyoun@gmail.com>
2022-09-28 19:54:28 +0900

    동적계획법

--- 백준 단계별로 풀어보기/동적 계획법 1/2579.py
+++ 백준 단계별로 풀어보기/동적 계획법 1/2579.py
@@ -1,29 +1,11 @@
 n = int(input())
-
-arr = []
-
+s = [0 for i in range(301)]
+dp = [0 for i in range(301)]
 for i in range(n):
-    arr.append(int(input()))
-    
-print(arr)
-arr2 = [0] *n
-arr2[0] = arr[0]
-count = 1
-for i in range(1, n):
-    
-    if i == 1:
-        arr2[1] = arr[0] + arr[1]
-        count += 1
-    else:
-        m = max(arr2[i-1],arr2[i-2])
-        if count == 2:
-            m = arr[i-2]
-        if m == arr[i-1]:
-            count += 1
-            arr[i] += m
-        else:
-            count = 0
-            arr[i] += m
-            
-    
-print(arr)
+    s[i] = int(input())
+dp[0] = s[0]
+dp[1] = s[0] + s[1]
+dp[2] = max(s[1] + s[2], s[0] + s[2])
+for i in range(3, n):
+    dp[i] = max(dp[i - 3] + s[i - 1] + s[i], dp[i - 2] + s[i])
+print(dp[n - 1])
