afe3dcc4dd146226de3d6d393df63103d258adb2
김성윤 <65865606+seongY0-0n@users.noreply.github.com>
2024-01-25 14:04:33 +0900

    [Silver III] Title: 로마 숫자 만들기, Time: 124 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/16922. 로마 숫자 만들기/로마 숫자 만들기.java
+++ 백준/Silver/16922. 로마 숫자 만들기/로마 숫자 만들기.java
@@ -1,32 +1,36 @@
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Scanner;
-import java.util.Set;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
+/**
+ * 1.문제설명: 1 5 10 50중에 중복을 허용해 n개를 뽑아 서로 다른 합의 갯수 구하기
+ * 2.코드 로직 및 구현과정: 트리 가지치기 그려보기 -> 백트래킹(종료 조건: depth == n이 되면 종료)
+ * 3.어려웠던 점: 단순히 모든 경우의 수를 구하려 하다보니 시간초과 발생 가능성이 생겼다.
+ *   for문에서 index 시작지점을 지정 why? 중복조합에서 경우의수를 뽑을 때를 생각해보면!
+ */
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        List<Integer> arr = new ArrayList<>();
-        arr.add(1);
-        arr.add(5);
-        arr.add(10);
-        arr.add(50);
-        Set<Integer> hset = new HashSet<>();
-        int[] roma = { 1, 5, 10, 50 };
-        for (int i = 0; i < N - 1; i++) {
-            Set<Integer> temp = new HashSet<>();
-            for (int j = 0; j < arr.size(); j++) {
-                for (int r : roma) {
-                    temp.add(arr.get(j) + r);
-                }
+    static int ans = 0;
+    static int[] list = new int[1001];
+    static int[] score = {1,5,10,50};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        back(0, 0, 0, n);
+        System.out.println(ans);
+    }
+
+    static void back(int index, int total, int depth, int end) {
+        if (depth == end) {
+            if (list[total] == 0){
+                list[total] = 1;
+                ans += 1;
             }
-            arr = new ArrayList<>(temp);
+            return;
         }
-
-        System.out.println(arr.size());
-
+        for (int i = index; i < 4; i++) {
+            back(i, total+score[i],depth +1, end);
+        }
     }
 }
