ea0077aa91455c638dba544c45365bdec739d9e9
김성윤 <65865606+seongY0-0n@users.noreply.github.com>
2024-02-04 19:40:20 +0900

    [Silver I] Title: 행운의 문자열, Time: 272 ms, Memory: 12156 KB -BaekjoonHub

--- 백준/Silver/1342. 행운의 문자열/행운의 문자열.java
+++ 백준/Silver/1342. 행운의 문자열/행운의 문자열.java
@@ -4,36 +4,38 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Main {
-    static char[] str;
-    static boolean[] isSelected;
-    static int count = 0;
+  static char[] str;
+  static boolean[] isSelected;
+  static int count = 0;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        str = br.readLine().toCharArray();
-        isSelected = new boolean[str.length];
-        Arrays.sort(str); // 사전순으로 출력하기 위해 미리 정렬
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    str = br.readLine().toCharArray();
+    StringBuilder sb = new StringBuilder();
+    isSelected = new boolean[str.length];
+    Arrays.sort(str);
 
-        perm(0, ' ');
-        System.out.println(count);
+    perm(0, ' ');
+    sb.append(count);
+    System.out.println(sb);
+  }
+
+  public static void perm(int depth, char prev) {
+    if (depth == str.length) {
+      count++;
+      return;
     }
 
-    public static void perm(int depth, char prev) {
-        if (depth == str.length) {
-            count++;
-            return;
+    for (int i = 0; i < str.length; i++) {
+      if (!isSelected[i] && str[i] != prev) {
+        if (i > 0 && str[i] == str[i - 1] && !isSelected[i - 1]) {
+          continue;
         }
 
-        for (int i = 0; i < str.length; i++) {
-            if (!isSelected[i] && str[i] != prev) {
-                if (i > 0 && str[i] == str[i - 1] && !isSelected[i - 1]) {
-                    continue; // 양 쪽에 같은 문자가 있는 경우 스킵
-                }
-
-                isSelected[i] = true;
-                perm(depth + 1, str[i]);
-                isSelected[i] = false;
-            }
-        }
+        isSelected[i] = true;
+        perm(depth + 1, str[i]);
+        isSelected[i] = false;
+      }
     }
+  }
 }
