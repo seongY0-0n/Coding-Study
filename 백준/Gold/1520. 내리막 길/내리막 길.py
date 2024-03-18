845cd536a5a70de0089c69cf362013afd4d37497
김성윤 <65865606+seongY0-0n@users.noreply.github.com>
2024-03-19 00:36:02 +0900

    [Gold III] Title: 내리막 길, Time: 172 ms, Memory: 43336 KB -BaekjoonHub

---
+++ 백준/Gold/1520. 내리막 길/내리막 길.py
@@ -0,0 +1,27 @@
+import sys
+sys.setrecursionlimit(10 ** 9)
+
+def dfs(x, y):
+
+    if x == m - 1 and y == n - 1:
+        return 1
+
+    if dp[x][y] == -1:
+        dp[x][y] = 0 
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < m and 0 <= ny < n:
+                if graph[x][y] > graph[nx][ny]:
+                    dp[x][y] += dfs(nx, ny) 
+    return dp[x][y]
+
+
+m, n = map(int, sys.stdin.readline().split())
+graph = [list(map(int, sys.stdin.readline().split())) for _ in range(m)]
+dp = [[-1 for _ in range(n)] for _ in range(m)]
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+print(dfs(0, 0))
