3e3165efa7b7b2271f048c2ebcaed811e9226ab1
김성윤 <65865606+seongY0-0n@users.noreply.github.com>
2024-04-01 15:28:41 +0900

    [Silver I] Title: 신입 사원, Time: 816 ms, Memory: 298784 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.java
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -9,37 +9,24 @@ import java.util.StringTokenizer;
 class Main {
 
     public static void main(String[] args) throws NumberFormatException, IOException {
-        class score {
-            int a, b;
 
-            public score(int a, int b) {
-                this.a = a;
-                this.b = b;
-            }
-
-        }
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
         for (int tc = 1; tc <= t; tc++) {
             int n = Integer.parseInt(br.readLine());
-            score[] scores = new score[n];
+            int[] score = new int[n];
             for (int i = 0; i < n; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
-                scores[i] = new score(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+                score[Integer.parseInt(st.nextToken()) - 1] = Integer.parseInt(st.nextToken()) - 1;
             }
-            Arrays.sort(scores, new Comparator<score>() {
-                @Override
-                public int compare(score o1, score o2) {
-                    return o1.a - o2.a;
-                }
-            });
+
             int ans = 0;
-            int temp = scores[0].b;
+            int temp = score[0];
             for (int i = 1; i < n; i++) {
-                if (scores[i].b > temp) {
+                if (score[i] > temp) {
                     ans++;
                 } else {
-                    temp = scores[i].b;
+                    temp = score[i];
                 }
             }
             System.out.println((n - ans));
