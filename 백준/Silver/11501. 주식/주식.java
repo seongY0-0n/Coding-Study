0804e85326ad4e6d17693c182b07ca3ee639ebc3
김성윤 <65865606+seongY0-0n@users.noreply.github.com>
2024-04-02 09:15:46 +0900

    [Silver II] Title: 주식, Time: 1108 ms, Memory: 319508 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.java
+++ 백준/Silver/11501. 주식/주식.java
@@ -16,31 +16,16 @@ public class Main {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int[] 주가 = new int[n];
             long ans = 0;
-            PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
 
-                @Override
-                public int compare(int[] o1, int[] o2) {
-                    return o2[1] - o1[1];
-                }
-
-            });
             for (int i = 0; i < n; i++) {
                 주가[i] = Integer.parseInt(st.nextToken());
-                pq.offer(new int[] { i, 주가[i] });
             }
-            int dDay = pq.peek()[0];
-            int price = pq.poll()[1];
-
-            for (int i = 0; i < n; i++) {
-                if (dDay > i) {
-                    ans += (price - 주가[i]);
+            int max = 주가[n - 1];
+            for (int i = n - 2; i >= 0; i--) {
+                if (주가[i] < max) {
+                    ans += max - 주가[i];
                 } else {
-                    while (!pq.isEmpty()) {
-                        dDay = pq.peek()[0];
-                        price = pq.poll()[1];
-                        if (dDay > i)
-                            break;
-                    }
+                    max = 주가[i];
                 }
             }
             System.out.println(ans);
