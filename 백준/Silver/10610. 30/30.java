9d9be0725ef3e1532b7b3e214af8f4a78a942992
김성윤 <65865606+seongY0-0n@users.noreply.github.com>
2024-04-08 08:59:30 +0900

    [Silver IV] Title: 30, Time: 120 ms, Memory: 15228 KB -BaekjoonHub

--- 백준/Silver/10610. 30/30.java
+++ 백준/Silver/10610. 30/30.java
@@ -1,37 +1,22 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String num = br.readLine();
-        int size = num.length();
-        int n[] = new int[size];
-        for (int i = 0; i < size; i++) {
-            n[i] = num.charAt(i) - '0';
-        }
-
-        Arrays.sort(n);
-        if (n[0] != 0) {
+        char[] num = br.readLine().toCharArray();
+        Arrays.sort(num);
+        if (num[0] != '0') {
             System.out.println(-1);
-        } else {
-            int temp = 0;
-            for (int i = 0; i < size; i++) {
-                temp += n[i];
-            }
-            if (temp % 3 != 0) {
-                System.out.println(-1);
-            } else {
-                String ans = "";
-                for (int i = size - 1; i >= 0; i--) {
-                    ans += (n[i]);
-                }
-                System.out.println(ans);
-            }
+            return;
         }
+        int sum = 0;
+        for (int i = 0; i < num.length; i++)
+            sum += num[i] - '0';
+        if (sum % 3 != 0)
+            System.out.println(-1);
+        else
+            System.out.println(new StringBuilder(String.valueOf(num)).reverse());
     }
 }
