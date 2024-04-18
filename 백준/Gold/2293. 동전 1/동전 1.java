ca588e792aa9604da07a8db4bce99c30825faac8
김성윤 <65865606+seongY0-0n@users.noreply.github.com>
2024-04-18 11:49:29 +0900

    [Gold V] Title: 동전 1, Time: 84 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -0,0 +1,26 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int coin[] = new int[n];
+        int dp[] = new int[k + 1];
+        dp[0] = 1;
+        for (int i = 0; i < n; i++) {
+            coin[i] = Integer.parseInt(br.readLine());
+            for (int j = coin[i]; j <= k; j++) {
+                dp[j] += dp[j - coin[i]];
+            }
+
+        }
+        System.out.println(dp[k]);
+    }
+}
