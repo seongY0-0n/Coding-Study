b316e399d5679e851dd2576844a60a30e77910e9
김성윤 <65865606+seongY0-0n@users.noreply.github.com>
2024-04-22 00:40:56 +0900

    [Gold V] Title: 빗물, Time: 92 ms, Memory: 11908 KB -BaekjoonHub

---
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int h = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        boolean[][] map = new boolean[h][w];
+
+        for (int c=0; c<w; c++) {
+            int b = Integer.parseInt(st.nextToken());
+            for (int r=0; r<b; r++) map[h-r-1][c] = true;
+        }
+
+        int result = 0;
+        for (int r=0; r<h; r++) {
+            int left = 0;
+            boolean flag = false;
+            for (int c=0; c<w; c++) {
+                if (map[r][c]) {
+                    if (flag) result += c-left-1;
+                    else flag = true;
+                    left = c;
+                }
+            }
+        }
+
+        System.out.println(result);
+    }
+}
