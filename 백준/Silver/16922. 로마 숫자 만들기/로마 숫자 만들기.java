db7249447a4b8f677c3ef229804aea3292428842
김성윤 <65865606+seongY0-0n@users.noreply.github.com>
2025-02-23 18:07:17 +0900

    [Silver III] Title: 로마 숫자 만들기, Time: 80 ms, Memory: 13116 KB -BaekjoonHub

--- 백준/Silver/16922. 로마 숫자 만들기/로마 숫자 만들기.java
+++ 백준/Silver/16922. 로마 숫자 만들기/로마 숫자 만들기.java
@@ -1,12 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashSet;
-import java.util.Scanner;
 import java.util.Set;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        sc.close();  // Scanner 닫기
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine()); 
+
 
         Set<Integer> current = new HashSet<>();
         current.add(1);
